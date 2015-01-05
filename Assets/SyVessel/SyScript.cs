5c78dde3aaa1b9b5f945da5ed360c53511943437
neoyoos <ksooyoun00@naver.com>
2015-01-05 14:40:21 +0900

    #2 SyVessel 만들었다

---
+++ Assets/SyVessel/SyScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyScript : MonoBehaviour {
+
+	public int hp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")) {
+			gameObject.transform.position += new Vector3(0, 0, 1);
+		}
+		if (Input.GetKey("a")) {
+			gameObject.transform.position += new Vector3(-1, 0, 0);
+		}
+		if (Input.GetKey("s")) {
+			gameObject.transform.position += new Vector3(0, 0, -1);
+		}
+		if (Input.GetKey("d")) {
+			gameObject.transform.position += new Vector3(1, 0, 0);
+		}
+	}
+
+	void Damage (int damage) {
+		hp -= damage;
+	}
+}
