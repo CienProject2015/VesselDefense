365d60fab070a7bb048a19f8aeed7d45c8ef7c62
dbsalsgur34 <dbsalsgur34@naver.com>
2015-01-05 20:16:38 +0900

    #3 'wasd'로 이동, 가감속, 마우스로 회전

---
+++ Assets/VesselCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VesselCamera : MonoBehaviour {
+	private float positionX;
+	private float positionZ;
+	private Vector3 cameraPosition;
+	private Vector3 cameraVelocity;
+	public GameObject myVessel;
+	// Use this for initialization
+	void Start () {
+		this.cameraPosition = new Vector3 (0, 10, -5);
+		this.cameraVelocity = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.rigidbody.transform.position = this.cameraPosition;
+		//this.rigidbody.velocity = this.cameraVelocity;
+		SetPosition ();
+	}
+
+	void SetPosition(){
+		this.cameraPosition = this.myVessel.rigidbody.position;
+		this.cameraPosition.y += 10.0f;
+		this.cameraPosition.z -= 5.0f;
+		this.rigidbody.position = this.cameraPosition;
+	}
+
+	void SetVelocity(Vector3 vel){
+		this.cameraVelocity = vel;
+	}
+
+}
