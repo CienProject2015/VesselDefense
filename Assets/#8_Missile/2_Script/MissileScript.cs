84babcb2633a21dd77a1acbeaed0ebf4bc2c8bb2
humit123 <dragon11shy@gmail.com>
2015-02-12 16:01:24 +0900

    #8 Missile - Guided Missile, missile rader의 collier radius 15 내 enemy 접근 시 유도탄 발사. 현재 collider는 1 layer만 적용됨. enemy 접근이 파악되면 enemy로 미사일 진행방향을 변경하는데 필요한 힘의 총합을 계산하여 적용함 ( wasd로 이동, 마우스 왼쪽 버튼 누를시 발사 ).
    * rader layer 수를 늘려 자연스러운 회전 구현 필요
    * 현재 collider collision 지점에서 기존 velocity를 0으로 만들고 이후 필요한 힘의 총합을 적용하는 방식인데 기존 velocity를 유지하는 공식으로 적용해보면 좀 더 자연스러운 회전이 가능할 것으로 보임

---
+++ Assets/#8_Missile/2_Script/MissileScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileScript : MonoBehaviour {
+
+	public float _timer; // time for script runtime
+	public float _timerForDel; // time for destroying object
+	private bool _raderCollChk; // collision check from the rader
+
+	// Use this for initialization
+	void Start () {
+	
+		GetComponent<Rigidbody>().isKinematic = false;
+		GetComponent<Rigidbody>().useGravity = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			if (_timer > _timerForDel) {
+
+				Destroy(gameObject);
+			}
+			else
+			{
+				_timer += Time.deltaTime;
+			    _raderCollChk = transform.Find("Rader").gameObject.GetComponent<MissileRader>()._collChk;
+			    if(_raderCollChk == false) {
+
+		    		rigidbody.AddForce(transform.up * 10.0f);
+					// magnitude is a temporary value
+					// this can change the velocity before collision, it works together with MissileRader
+
+			    }
+			    else {
+
+					rigidbody.velocity = new Vector3(0,0,0);
+
+			    }
+			}
+	}
+		
+	void OnTriggerEnter(Collider other) {
+		if(other.transform.tag == "Enemy") { 
+
+			Destroy(gameObject);
+
+	    }
+	}
+
+}
---
+++ Assets/#8_Missile/2_Script/PlayerScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	
+	// Player's basic properties	
+	public float _speed; // player's move speed	
+	public float _hp; // player's hp
+	public bool _playerLive; // bool for player's live
+	public bool _attackChk;
+
+	
+	// Use this for initialization
+	void Start () { 
+		_speed = 5.0f;
+		_playerLive = true;
+		_attackChk = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_playerLive)
+		{
+
+			if ((Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0))
+			{
+				float _verticalPos = Input.GetAxis("Vertical") * _speed * Time.deltaTime * 50;
+				float _horizonPos = Input.GetAxis("Horizontal") * _speed * Time.deltaTime * 20;
+
+				transform.Rotate(0, _horizonPos, 0);
+				rigidbody.AddForce(transform.forward * _verticalPos); //(new Vector3(_horizonPos, 0, _verticalPos));
+				//transform.localPosition += new Vector3(0, 0, _verticalPos);
+
+			}
+
+			if (Input.GetButtonDown("Fire1"))
+			{
+				_attackChk = true;
+			}else{
+				//_attackChk = false;
+			}
+			//_attackChk = false;
+		}
+	}
+	
+	void Damaged(float _dam)
+	{
+		_hp -= _dam;
+		
+		if(_hp >0)
+		{
+
+		}
+		else if(_hp <= 0)
+		{
+			_playerLive=false;
+		}
+	}
+}
